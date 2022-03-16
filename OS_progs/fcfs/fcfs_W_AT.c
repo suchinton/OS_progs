50e92ba416df52f895f97c066d85c8009db8eb74
suchinton <suchinton.chakravarty@s.amity.edu>
2022-03-16 20:56:01 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   OS_progs/fcfs/fcfs_WO_AT.c
    	renamed:    OS_progs/fcfs_W_AT -> OS_progs/fcfs/fcfs_W_AT
    	new file:   OS_progs/fcfs/fcfs_W_AT.c
    	deleted:    OS_progs/fcfs_W_AT.c
    	deleted:    OS_progs/sjf/sjf.java

--- OS_progs/fcfs/fcfs_W_AT.c
+++ OS_progs/fcfs/fcfs_W_AT.c
@@ -14,7 +14,7 @@ void findTurnAroundTime( int processes[], int n, int bt[], int wt[], int tat[])
 		tat[i] = bt[i] + wt[i];
 }
 
-void findavgTime( int processes[], int n, int bt[])
+void findavgTime( int processes[],int arrival_time[], int n, int bt[])
 {
 	int wt[n], tat[n], total_wt = 0, total_tat = 0;
 
@@ -22,13 +22,13 @@ void findavgTime( int processes[], int n, int bt[])
 
 	findTurnAroundTime(processes, n, bt, wt, tat);
 
-	    printf("| Processes | Burst time | Waiting time | Turn around time |\n");
+	    printf("| Processes | Arrival time | Burst time | Waiting time | Turn around time |\n");
 
 	for (int i=0; i<n; i++)
 	{
 		total_wt = total_wt + wt[i];
 		total_tat = total_tat + tat[i];
-		printf("|  %d        |    %d      |     %d       |     %d           |\n",(i+1),bt[i],wt[i],tat[i] );
+		printf("|  %d        |      %d      |    %d      |     %d       |     %d           |\n",processes[i],arrival_time[i],bt[i],wt[i],tat[i] );
 	}
 	float s=(float)total_wt / (float)n;
 	float t=(float)total_tat / (float)n;
@@ -46,15 +46,33 @@ int main()
 
 	int processes[n];
 	int burst_time[n];
-    
+	int arrival_time[n];
+    int temp;
+
     for(int i=0; i<n; i++)
     {
         processes[i] = i+1;
         printf("\nProcess %d : burst time : ",i+1);
         scanf("%d", &burst_time[i]);
+		printf("\nArrival time : ");
+		scanf("%d",&arrival_time[i]);
     }
+
+	for(int i=0; i<n; i++)
+	{
+		if(arrival_time[i]<arrival_time[i+1])
+		{
+			temp = processes[i];
+			processes[i] = processes[i+1];
+			processes[i+1] = processes[i];
+
+			temp = burst_time[i];
+			burst_time[i] = burst_time[i+1];
+			burst_time[i+1] = burst_time[i];
+		}
+	}
 	
-    findavgTime(processes, n, burst_time);
+    findavgTime(processes,arrival_time, n, burst_time);
 	return 0;
 }
 
